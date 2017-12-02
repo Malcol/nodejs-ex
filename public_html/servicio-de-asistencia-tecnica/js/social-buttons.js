8b190676fb288a7d5083ecbec5fdff0507364e5f
lmiguel <malcolfo@gmail.com>
2017-12-02 12:37:21 +0100

    directorio

---
+++ public_html/servicio-de-asistencia-tecnica/js/social-buttons.js
@@ -0,0 +1,260 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
+    	<!-- Imports Header -->
+	    
+    <title>RM Informáticos | Servicio de Asistencia Técnica Informática</title>	
+    <meta name="description" content="RM Informáticos | Somos técnicos especializados en reparación de equipos informáticos, impresoras y redes." />
+	<meta name="keywords" content="informáticos, informática, formación, seguridad, web, diseño web, aplicaciones móviles, mantenimiento informático, gestión de datos, aplicaciones cloud, aplicaciones en la nube, plataformas cloud, plataformas en la nube, gestión educativa, gestión escolar, gestión académica, gestión médica, gestión centros médicos, software académico, software médico, programa facturación, software facturas, gestión integral, clases de repaso, apoyo escolar, apoyo universitario, clases particulares" />
+    <meta name="google-site-verification" content="ErPOZywrYy1ctzWixrRcGHEPgKiewCC9ZjsUENsomiM" />
+	<meta name="robots" content="index,follow">
+	<link rel="canonical" href="http://www.rminformaticos.com/" />
+	<link rel="profile" href="http://gmpg.org/xfn/11" />
+
+	<meta property="og:title" content="RM Informáticos | Servicio de Asistencia Técnica Informática" />
+	<meta property="og:url" content="http://www.rminformaticos.com/servicio-de-asistencia-tecnica/js/social-buttons.js" />
+	<meta property="og:title" content="RM Informáticos | Servicio de Asistencia Técnica Informática" />
+	<meta property="og:type" content="website" />
+	<meta property="og:url" content="http://www.rminformaticos.com/servicio-de-asistencia-tecnica/js/social-buttons.js" />
+	<meta property="og:image" content="http://www.rminformaticos.com/img/ico/apple-touch-icon-144x144.png" />
+	<meta property="og:site_name" content="RM Informáticos" />
+	<meta property="og:description" content="RM Informáticos | Somos técnicos especializados en reparación de equipos informáticos, impresoras y redes." />
+        
+	<meta name="twitter:card" content="summary" />
+	<meta name="twitter:description" content="RM Informáticos | Somos técnicos especializados en reparación de equipos informáticos, impresoras y redes." />
+	<script>
+	  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+	  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+	  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+	  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+	  ga('create', 'UA-21235582-1', 'rminformaticos.com');
+	  ga('send', 'pageview');
+
+	</script>
+	
+    <!-- Bootstrap -->
+    <link rel="stylesheet" href="/css/bootstrap.min.css">
+    <!-- Animation Libraries -->
+    <link rel="stylesheet" href="/css/animate.min.css">
+    <link rel="stylesheet" href="/css/magic.min.css">
+    <!-- Google Fonts -->
+    <link href='http://fonts.googleapis.com/css?family=Lato:100,300,400,700,900%7COpen+Sans:300,400,600,700' rel='stylesheet' type='text/css'>
+    <!-- Fonts Icons -->
+    <link rel="stylesheet" href="/css/custom-icons.css">
+    <link rel="stylesheet" href="/css/et-line-icons.css">
+    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
+    <!-- Owl Carousel -->
+    <link rel="stylesheet" href="/css/owl.carousel.min.css">
+    <!-- Nivo Lightbox -->
+    <link rel="stylesheet" href="/css/nivo-lightbox/nivo-lightbox.css">
+    <link rel="stylesheet" href="/css/nivo-lightbox/lightbox-theme.css">
+    <!-- Animated Headlines -->
+    <link rel="stylesheet" href="/css/animated-hedlines.css">
+    <!-- Main Styles -->
+    <link href="/css/styles.css" rel="stylesheet">
+    <!-- Theme -->
+    <link href="/css/theme/orange.css" rel="stylesheet" data-color="" id="theme">
+    <!-- Animsition -->
+    <link href="/css/animsition.min.css" rel="stylesheet">
+    <!-- Social -->
+    <link rel="stylesheet" href="/css/social-buttons.css">
+    <!-- Favicon (retina) -->
+    <link rel="shortcut icon" href="/img/ico/favicon.ico" type="image/x-icon">
+	<link rel="apple-touch-icon" href="/img/ico/apple-touch-icon.png" />
+	<link rel="apple-touch-icon" sizes="57x57" href="/img/ico/apple-touch-icon-57x57.png">
+	<link rel="apple-touch-icon" sizes="72x72" href="/img/ico/apple-touch-icon-72x72.png">
+	<link rel="apple-touch-icon" sizes="114x114" href="/img/ico/apple-touch-icon-114x114.png">
+	<link rel="apple-touch-icon" sizes="144x144" href="/img/ico/apple-touch-icon-144x144.png">    		
+</head>
+
+<body>
+    <!-- Page Wrapper -->
+    <div id="page">
+        
+		<!-- Navbar -->
+		        
+        <div class="position-relative">            
+            <div id="sticky-navbar">
+                <nav id="nav" class="navbar navbar-fixed-top navbar-dark ripple-group-parent init-animation-1 fadeInDown" role="navigation">                    
+                    <div class="container in-page-scroll">
+                        <!-- Brand and toggle get grouped for better mobile display -->
+                        <div class="navbar-header">
+                            <button type="button" id="animated-navicon" class="navbar-toggle" data-toggle="collapse" data-target="#mobile-navbar-collapse">
+                                <span class="sr-only">Menu</span>
+                                <span class="icon-bar"></span>
+                                <span class="icon-bar"></span>
+                                <span class="icon-bar"></span>
+                            </button>
+                            <a class="navbar-brand ripple-group" href="/#inicio">
+                                <div class="navbar-logo pull-left">
+                                    <img src="/img/logo.png" alt="logo" style="width:220px; height:33px;" />
+                                </div>
+                            </a>
+                        </div>
+                        <!-- Collect the nav links, forms, and other content for toggling -->
+                        <div class="collapse navbar-collapse" id="mobile-navbar-collapse">
+                            <ul class="nav navbar-nav navbar-right cl-effect-5">
+                                <li>
+                                    <a class="ripple-group" href="/#inicio">
+                                        <span data-hover="inicio">inicio</span>
+                                    </a>
+                                </li>
+								<li class="active">
+                                    <a class="ripple-group" href="/#servicios">
+                                        <span data-hover="servicios">servicios</span>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a class="ripple-group" href="/#empresa">
+                                        <span data-hover="empresa">empresa</span>
+                                    </a>
+                                </li>                                
+                                <li>
+                                    <a class="ripple-group" href="/#contacto">
+                                        <span data-hover="contacto">contacto</span>
+                                    </a>
+                                </li>
+                            </ul>
+                        </div>
+                        <!-- /.navbar-collapse -->
+                    </div>
+                    <!-- /.container -->
+                </nav>
+            </div>
+        </div>
+        <!--/ End Navbar -->		
+		<div id="page-2">					
+            <section id="servicios" class="services-section section" style="padding-top:43px !important;">
+				<div>
+					<ol class="breadcrumb  text-right">
+						<li class="init-animation-3 delay0-4s"><a class="animsition-link" href="http://www.rminformaticos.com/#inicio">Inicio</a>
+						</li>
+						<li class="init-animation-3 delay0-6s"><a class="animsition-link" href="javascript:history.back()">Servicios</a>
+						</li>
+						<li class="active init-animation-3 delay0-8s">Servicio de Asistencia Técnica</li>
+					</ol>
+				</div>
+                <div class="container">
+                    <div class="section-title">
+                        <div>
+                            <h1 class="section-title-heading"><span>Servicio de Asistencia Técnica</span></h1>
+                        </div>
+                    </div>
+					
+                    <div class="services-section-items padding-top-sm-10 row">
+                        <div class="col-sm-12">
+							<p style="text-align: justify;">Si por las características de su empresa no requiere de un servicio avanzado como el mantenimiento integral, RM Informáticos puede realizar intervenciones bajo demanda cuando usted lo necesite. Estas podrán ser tanto en su empresa como en nuestro propio taller de reparación y montaje.</p>
+						</div>
+						<div class="col-sm-6">
+							<div id="images-slider-1" class="owl-carousel owl-theme">
+							  <div class="item"><img src="http://www.rminformaticos.com/img/services/sat_1.jpg" alt=""></div>
+							  <div class="item"><img src="http://www.rminformaticos.com/img/services/sat_2.jpg" alt=""></div>
+							  <div class="item"><img src="http://www.rminformaticos.com/img/services/sat_3.jpg" alt=""></div>							 
+							</div>
+						</div>
+						<div class="col-sm-6">
+							<p style="text-align: justify;">Disponemos de un equipo de asistencia técnica altamente cualificado y eficaz con tiempos de respuesta prácticamente inmediatos y capaz de encontrar y resolver sus problemas sin necesidad de que prescinda de su equipo y en el caso de que sea necesaria su retirada pueden ser capaces de detectar y resolver su problema en menos de 24 horas.</p>
+							<p style="text-align: justify;">Nuestro servicio de <strong>Asistencia Técnica Remota</strong> es un innovador sistema que permitirá a nuestros técnicos conectarse de forma inmediata a sus sistemas para solucionar cualquier problema, fallo o configuración de software que requieran, siempre previa autorización suya.</p>
+							<p style="text-align: justify;"><strong>RM Informáticos</strong> ofrece a sus clientes el servicio de asistencia remota desde nuestras instalaciones. Esto nos permite de forma rápida, eficaz y económica, resolver los problemas de software de sus sistemas y servidores.</p>							
+						</div>
+                    </div>
+                    <!--/.row -->
+                </div>
+                <!--/.container -->
+            </section>
+            <!--/ End Services Section -->
+            <hr />
+			
+            <!-- Footer Section -->
+            			<!-- Footer Section -->
+            <footer class="footer-section section no-padding-bottom padding-top-30">
+                <!-- go top pop-up -->
+                <div id="go-top" class="go-top go-top-out in-page-scroll">
+                    <div class="circle1"></div>
+                    <a href="#inicio" class="square"><span></span></a>
+                    <div class="rectangle"></div>
+                    <div class="circle2"></div>
+                </div>
+                <!--/ End go top pop-up -->
+                <div class="footer-bottom">
+					<!-- Copyright -->
+                    <div class="footer-bottom-text font-second">
+                        RM Informáticos © 2016. 
+                        <a href="/legal/" >Aviso legal.</a><br>
+                          <div class="social-sharing" data-permalink="http://www.rminformaticos.com/servicio-de-asistencia-tecnica/js/social-buttons.js">
+
+                            <!-- https://developers.facebook.com/docs/plugins/share-button/ -->
+                            <a target="_blank" href="http://www.facebook.com/sharer.php?u=http://www.rminformaticos.com/servicio-de-asistencia-tecnica/js/social-buttons.js" class="share-facebook">
+                              <i class="fa fa-facebook icon"></i>
+                              <span class="share-title">Compartir</span>
+                            </a>
+
+                            <!-- https://dev.twitter.com/docs/intents -->
+                            <a target="_blank" href="http://twitter.com/share?url=http://www.rminformaticos.com/servicio-de-asistencia-tecnica/js/social-buttons.js;text=Visita%20la%20web%20de%20RM%20Informáticos&" class="share-twitter">
+                              <i class="fa fa-twitter icon"></i>
+                              <span class="share-title">Tweet</span>
+                            </a>
+                              
+                            <a target="_blank" href="https://twitter.com/intent/follow?original_referer=http://www.rminformaticos.com/servicio-de-asistencia-tecnica/js/social-buttons.js&ref_src=twsrc%5Etfw&region=follow_link&screen_name=RM_Informaticos&tw_p=followbutton" class="share-twitter">
+                                <i class="fa fa-twitter icon"></i>
+                                <span class="share-title">Seguir a @RM_Informaticos</span>
+                            </a>  
+
+                            <!-- https://developers.google.com/+/web/share/ -->
+                            <a target="_blank" href="http://plus.google.com/share?url=http://www.rminformaticos.com/servicio-de-asistencia-tecnica/js/social-buttons.js" class="share-google">
+                              <i class="fa fa-google-plus icon"></i>
+                              <span class="share-title">+1</span>
+                            </a>
+
+                            <!-- https://developer.linkedin.com/plugins/share -->
+                            <a target="_blank" href="http://www.linkedin.com/shareArticle?mini=true&url=http://www.rminformaticos.com/servicio-de-asistencia-tecnica/js/social-buttons.js&title=RM%20Informaticos" class="share-linkedin">
+                              <i class="fa fa-linkedin icon"></i>
+                              <span class="share-title">Compartir</span>
+                            </a>
+
+
+                          </div>
+                    </div>
+                    <!-- End Copyright -->
+                </div>
+            </footer>
+            <!--/ End Footer Section -->        </div>
+        
+    </div>
+    <!--/ #page -->
+	
+	<!-- JS Scripts -->
+	
+    <script src="/js/modernizr.min.js"></script>
+    <script src="/js/jquery-2.1.1.min.js"></script>
+    <script src="/js/jquery.easing.1.3.min.js"></script>
+    <script src="/js/jquery.animsition.min.js"></script>
+    <script src="/js/smoothscroll.js"></script>
+    <script src="/js/skrollr.min.js"></script>
+    <script src="/js/wow.min.js"></script>
+    <script src="/js/waypoints.min.js"></script>
+    <script src="/js/bootstrap.min.js"></script>
+    <script src="/js/jquery.counterup.min.js"></script>
+    <script src="/js/owl.carousel.min.js"></script>
+    <script src="/js/animateSlider.js"></script>
+    <script src="/js/animated-headlines.js"></script>
+    <script src="/js/jquery.mb.YTPlayer.min.js"></script>
+    <script src="/js/jquery.vide.min.js"></script>
+    <script src="/js/jquery.particleground.min.js"></script>
+    <script src="/js/fss.min.js"></script>
+    <script src="/js/fssinit.js"></script>
+    <script src="/js/retina.min.js"></script>
+    <script src="/js/isotope.pkgd.min.js"></script>
+    <script src="/js/nivo-lightbox.min.js"></script>
+    <script src="/js/contactForm.js"></script>
+    <script src="js/social-buttons.js"></script>
+    <script src="/js/script.min.js"></script>
+    <!--/ End JS Scripts -->    
+</body>
+
+</html>
